656581fb88d41de20e2b003ac62f07e5344cc885
Joydeep Biswas <joydeepb@cs.utexas.edu>
2020-03-24 12:58:30 -0500

    Convert hardcoded paths to CLI args

--- data_preprocessing/scripts/run_airsim_data_processing.bash
+++ data_preprocessing/scripts/run_airsim_data_processing.bash
@@ -3,20 +3,20 @@
 
 export ROS_PACKAGE_PATH=`pwd`/..:$ROS_PACKAGE_PATH
 
+if [ -z "$1" ] || [ -z "$2" ] [ -z "$3" ]
+then
+  echo "USAGE: $0 SOURCE_DIR OUT_DIR SESSION_NUM"
+fi
 
-SESSIONS="3"
+CAM_EXTRINSICS_PATH="../util/Camera_Extrinsics.yaml"
 
-
-CAM_EXTRINSICS_PATH="/home/srabiee/My_Repos/IVOA/data_preprocessing/"\
-"util/Camera_Extrinsics.yaml"
-
-SOURCE_DIR="/media/ssd2/datasets/AirSim_IVOA/initial_ml_tool"
-OUTPUT_DATASET_DIR="/media/ssd2/datasets/AirSim_IVOA/airsim_ivoa_test/"
-
+SOURCE_DIR=$1
+OUTPUT_DATASET_DIR=$2
+SESSIONS=$3
 
 for session in $SESSIONS; do
   printf -v SESSION_NUM_STR '%05d' "$session"
-  
+
   echo "*********************************"
   echo "Generating Dataset Session $SESSION_NUM_STR"
   echo "*********************************"
@@ -25,7 +25,7 @@ for session in $SESSIONS; do
   --session_num=$session \
   --source_dir=$SOURCE_DIR \
   --cam_extrinsics_path=$CAM_EXTRINSICS_PATH \
-  --output_dataset_dir=$OUTPUT_DATASET_DIR 
-  
+  --output_dataset_dir=$OUTPUT_DATASET_DIR
+
 done
 
